f7846cb830846d75cf3186dc19d50a4af2968ec4
Nikita Bosov <neckbosov@gmail.com>
2021-05-23 23:34:48 +0300

    Fix test paths

--- test/test_cbs.cpp
+++ test/test_cbs.cpp
@@ -48,26 +48,26 @@ TEST(CBSTest, CBSSimple) {
 }
 
 TEST(CBSTest, CBSBostonSmall1) {
-    CBSTest::testCBS("../data/maps/mapf/maze-32-32-2.map",
-                     "../data/scens/mapf/maze-32-32-2-even-1.scen",
+    CBSTest::testCBS("../data/maps/mapf/Boston.map",
+                     "../data/scens/mapf/Boston.scen",
                      0, 5);
 }
 
 TEST(CBSTest, CBSBostonSmall2) {
-    CBSTest::testCBS("../data/maps/mapf/maze-32-32-2.map",
-                     "../data/scens/mapf/maze-32-32-2-even-1.scen",
+    CBSTest::testCBS("../data/maps/mapf/Boston.map",
+                     "../data/scens/mapf/Boston.scen",
                      10, 15);
 }
 
 TEST(CBSTest, CBSBostonSmall3) {
-    CBSTest::testCBS("../data/maps/mapf/maze-32-32-2.map",
-                     "../data/scens/mapf/maze-32-32-2-even-1.scen",
+    CBSTest::testCBS("../data/maps/mapf/Boston.map",
+                     "../data/scens/mapf/Boston.scen",
                      300, 306);
 }
 
 TEST(CBSTest, CBSBostonSmall4) {
-    CBSTest::testCBS("../data/maps/mapf/maze-32-32-2.map",
-                     "../data/scens/mapf/maze-32-32-2-even-1.scen",
+    CBSTest::testCBS("../data/maps/mapf/Boston.map",
+                     "../data/scens/mapf/Boston.scen",
                      350, 357);
 }
 
